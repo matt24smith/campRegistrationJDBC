c3132f514db9aeea155767b1dd56e194bec4371f
Matt Smith <matt24smith@gmail.com>
2016-06-22 11:59:53 -0300

    last commit before lunch

--- src/campRegistrationJDBC/Camps2CampersUI.java
+++ src/campRegistrationJDBC/Camps2CampersUI.java
@@ -1,5 +1,25 @@
 package campRegistrationJDBC;
 
+import java.awt.FlowLayout;
+
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 public class Camps2CampersUI {
-	//this is gonna be a doozie 
+
+	private JComboBox selectCamper = new JComboBox();
+	private JComboBox selectCamp = new JComboBox();
+	
+	private JButton refresh = new JButton("Refresh");
+	private JButton registerCamper = new JButton("Register");
+	
+	private JTextField registrationDiscount;
+	
+	private Camps2CampersBean bean = new Camps2CampersBean();
+	
+	public Camps2CampersUI(){
+		
+	}
 }
